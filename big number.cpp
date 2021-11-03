d0d97f50260edd7d618b304a53f66b808f017417
turkalpp <71527921+turkalpp@users.noreply.github.com>
2021-11-03 21:52:27 +0300

    Add files via upload

---
+++ big number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	int x,y;
+	cout<<" Enter a number: ";
+	cin>>x;
+	
+	cout<<" Enter a number: ";
+	cin>>y;
+	
+	if(x>y)
+	{
+		cout<<" First number is big.";
+	}
+	else
+	{
+		cout<<" Second number is big. ";
+	}
+}
